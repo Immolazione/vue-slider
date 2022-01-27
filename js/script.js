5c8e40c5ae5860b139d0c16f4836b6a419bc2e29
Immolazione <federico.deered@gmail.com>
2022-01-27 16:36:53 +0100

    add imgs array

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
 console.log('VUE.JS OK', Vue);
 
+Vue.config.devtools = true;
+
+/*
+Descrizione:
+Partendo dal markup fornito in allegato, implementare la logica per far funzionare lo slider:
+Deve essere possibile scorrere le immagini cliccando sulle freccette.
+I pallini in basso devono illuminarsi in base all'immagine che sto visualizzando.
+Far si che cliccando i pallini, venga selezionata l'immagine corrispondente.
+Note:  scegliamo una delle due seguenti soluzioni per le freccette:
+Se stiamo visualizzando la prima immagine, la freccetta di sinistra può:
+scomparire
+passare all'ultima immagine
+Se stiamo visualizzando l'ultima immagine, la freccetta  a destra può:
+scomparire
+passare alla prima immagine
+Siate coerenti: o scompaiono o continuano a navigare, no a soluzioni miste!!
+*/
+
 const app = new Vue (
     {
         el: '#root',
+        data: {
+            images: ['image1.jpg', 
+            'image2.jpg', 
+            'image3.jpg', 
+            'image4.jpg'],
+
+        },
     },
 );
